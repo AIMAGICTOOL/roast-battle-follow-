33ade88a7ebc280867be468496d27710867b26de
AIMAGICTOOL <shivanshuarya95@gmail.com>
2026-09-18 16:00:25 +0530

    Enhance index.js with comments for clarity
    Added comments for static middleware and fallback route.

--- api/index.js
+++ api/index.js
@@ -3,9 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.json());
+// Express static middleware to serve CSS & JS
 app.use(express.static(path.join(__dirname, '../public')));
 
+// Fallback to serve index.html for all routes
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
